888a4fb14d7e18d153ef909b4910126b6e236453
aryan kapoor <aryankapoor@aryans-MacBook-Neo.local>
2026-08-18 11:58:13 +0530

    aryanabc

---
+++ script.js
@@ -0,0 +1,334 @@
+// Catalog Data Arrays with Specific Image Paths
+const batteryParts = [
+    { name: "Double Motor Heavy", image: "images/battery/part_1.jpg" },
+    { name: "Single Motor Heavy", image: "images/battery/part_2.jpg" },
+    { name: "Digital Charger Extra Heavy", image: "images/battery/part_3.jpg" },
+    { name: "Heavy Charger 2 Mtr Wire", image: "images/battery/part_4.jpg" },
+    { name: "Medium Charger (Orange Colour)", image: "images/battery/part_5.jpg" },
+    { name: "Regulator Heavy Blue (AK)", image: "images/battery/part_6.jpg" },
+    { name: "Regulator Medium Blue (AK)", image: "images/battery/part_7.jpg" },
+    { name: "Regulator Small & Medium Black", image: "images/battery/part_8.jpg" },
+    { name: "Delivery Pipe", image: "images/battery/part_9.jpg" },
+    { name: "Delivery Pipe Extra Heavy", image: "images/battery/part_10.jpg" },
+    { name: "Italian Gun", image: "images/battery/part_11.jpg" },
+    { name: "60cm Band Brass Gun", image: "images/battery/part_12.jpg" },
+    { name: "60cm Brass Gun", image: "images/battery/part_13.jpg" },
+    { name: "90cm Brass Gun", image: "images/battery/part_14.jpg" },
+    { name: "90cm Band Brass Gun", image: "images/battery/part_15.jpg" },
+    { name: "5 Hole Nozzle with SS Plate", image: "images/battery/part_16.jpg" },
+    { name: "5 Hole Nozzle", image: "images/battery/part_17.jpg" },
+    { name: "8 Hole Nozzle Extra Heavy", image: "images/battery/part_18.jpg" },
+    { name: "Big Hood Nozzle", image: "images/battery/part_19.jpg" },
+    { name: "Medium Hood Nozzle", image: "images/battery/part_20.jpg" },
+    { name: "Adjustable Ban Nozzle", image: "images/battery/part_21.jpg" },
+    { name: "Single Hole Band Nozzle", image: "images/battery/part_22.jpg" },
+    { name: "2 Hole F Nozzle", image: "images/battery/part_23.jpg" },
+    { name: "3 Hole F Nozzle", image: "images/battery/part_24.jpg" },
+    { name: "SS Lance", image: "images/battery/part_25.jpg" },
+    { name: "Trigger Heavy", image: "images/battery/part_26.jpg" },
+    { name: "Gun Handle Plastic Thread New", image: "images/battery/part_27.jpg" },
+    { name: "Both Side Thread Handle", image: "images/battery/part_28.jpg" },
+    { name: "Gun Handle with Brass Nut", image: "images/battery/part_29.jpg" },
+    { name: "Buckle Set 2 Inches", image: "images/battery/part_30.jpg" },
+    { name: "Buckle Set 1.5 Inches", image: "images/battery/part_31.jpg" },
+    { name: "2 and 1.5 Waterproof Belt Set Metal Buckle", image: "images/battery/part_32.jpg" },
+    { name: "2 Inches Belt Set Leather Cushion Special", image: "images/battery/part_33.jpg" },
+    { name: "Tank Cap Big", image: "images/battery/part_34.jpg" },
+    { name: "Tank Cap Small", image: "images/battery/part_35.jpg" },
+    { name: "Tank Strainer", image: "images/battery/part_36.jpg" },
+    { name: "Transparent Adj. Nozzle", image: "images/battery/part_37.jpg" },
+    { name: "Double Switch with Wire", image: "images/battery/part_38.jpg" },
+    { name: "Single Switch with Wire", image: "images/battery/part_39.jpg" },
+    { name: "Charging Socket with Wire", image: "images/battery/part_40.jpg" },
+    { name: "2 Way Connection", image: "images/battery/part_41.jpg" },
+    { name: "Motor Head Kit", image: "images/battery/part_42.jpg" },
+    { name: "Pressure Washer with Double Taper", image: "images/battery/part_43.jpg" },
+    { name: "Pressure Washer with Single Taper", image: "images/battery/part_44.jpg" },
+    { name: "Small Elbow with Clip", image: "images/battery/part_45.jpg" },
+    { name: "Big Elbow with Clip", image: "images/battery/part_46.jpg" },
+    { name: "Gun Cap Set", image: "images/battery/part_47.jpg" },
+    { name: "Gun Handle", image: "images/battery/part_48.jpg" },
+    { name: "Small Switch", image: "images/battery/part_49.jpg" },
+    { name: "Black Switch for LED", image: "images/battery/part_50.jpg" },
+    { name: "Tank Filter", image: "images/battery/part_51.jpg" },
+    { name: "Operating Handle Set Small and Big", image: "images/battery/part_52.jpg" },
+    { name: "Volt Meter", image: "images/battery/part_53.jpg" },
+    { name: "Hylozen", image: "images/battery/part_54.jpg" },
+    { name: "Hylozen Square", image: "images/battery/part_55.jpg" },
+    { name: "LED 5W / 7W / 9W", image: "images/battery/part_56.jpg" },
+    { name: "Battery Clip 12/12", image: "images/battery/part_57.jpg" },
+    { name: "Battery Clip 8/12", image: "images/battery/part_58.jpg" },
+    { name: "12mm Clip", image: "images/battery/part_59.jpg" },
+    { name: "18mm Clip", image: "images/battery/part_60.jpg" },
+    { name: "Wire Clip", image: "images/battery/part_61.jpg" },
+    { name: "Handle Lock", image: "images/battery/part_62.jpg" }
+];
+
+const htpParts = [
+    { name: "Footvalve Filter Regular", image: "images/htp/part_1.jpg" },
+    { name: "Footvalve Filter Big", image: "images/htp/part_2.jpg" },
+    { name: "U Type Filter 19mm", image: "images/htp/part_3.jpg" },
+    { name: "U Type Filter 32mm", image: "images/htp/part_4.jpg" },
+    { name: "Air Chamber Long and Regular", image: "images/htp/part_5.jpg" },
+    { name: "Plunger for 30/50 and 80 No.", image: "images/htp/part_6.jpg" },
+    { name: "Mixer", image: "images/htp/part_7.jpg" },
+    { name: "Regulator Assy. 80 No.", image: "images/htp/part_8.jpg" },
+    { name: "Regulator Assy. 30/50 No.", image: "images/htp/part_9.jpg" },
+    { name: "Complete Regulator Assy. for 30/50/80", image: "images/htp/part_10.jpg" },
+    { name: "Wire Pipe for Suction 30/50/80", image: "images/htp/part_11.jpg" },
+    { name: "Delivery Hose 8.5mm", image: "images/htp/part_12.jpg" },
+    { name: "Delivery Hose 10mm", image: "images/htp/part_13.jpg" },
+    { name: "Connecting Rod 30/50/80", image: "images/htp/part_14.jpg" },
+    { name: "Crankshaft for 30/50/80", image: "images/htp/part_15.jpg" },
+    { name: "Aluminium Pulley", image: "images/htp/part_16.jpg" },
+    { name: "Pressure Watch 30/50/80 No.", image: "images/htp/part_17.jpg" },
+    { name: "Pressure Ball Set 30/50/80 No.", image: "images/htp/part_18.jpg" },
+    { name: "Grease Ring 22/30/50/80/120", image: "images/htp/part_19.jpg" },
+    { name: "Gland 22/30/50/80/120", image: "images/htp/part_20.jpg" },
+    { name: "Nut Nipple Overflow and Suction 22/30/50/80", image: "images/htp/part_21.jpg" },
+    { name: "Oil Cap Long Type", image: "images/htp/part_22.jpg" },
+    { name: "Oil Glass", image: "images/htp/part_23.jpg" },
+    { name: "Washer for Suction All Type", image: "images/htp/part_24.jpg" },
+    { name: "Washer for Delivery All Type", image: "images/htp/part_25.jpg" },
+    { name: "Grease Cup", image: "images/htp/part_26.jpg" },
+    { name: "Bush All Size Available", image: "images/htp/part_27.jpg" },
+    { name: "Oil Cap Red", image: "images/htp/part_28.jpg" },
+    { name: "Hard Ring 22/30/50/80/120", image: "images/htp/part_29.jpg" },
+    { name: "Oil Seal", image: "images/htp/part_30.jpg" },
+    { name: "Valve Assy. Black 22/30/50/80/120", image: "images/htp/part_31.jpg" },
+    { name: "Valve Assy. Red", image: "images/htp/part_32.jpg" },
+    { name: "V Packing for 22/30/50/80/120", image: "images/htp/part_33.jpg" },
+    { name: "Valve Packing 22/30/50/80/120", image: "images/htp/part_34.jpg" }
+];
+
+const tu26Parts = [
+    { name: "Carburetor TU-26", image: "images/tu26/part_1.jpg" },
+    { name: "Cylinder Assembly TU-26", image: "images/tu26/part_2.jpg" },
+    { name: "Ignition Coil TU-26", image: "images/tu26/part_3.jpg" },
+    { name: "Piston Assembly for TU-26", image: "images/tu26/part_4.jpg" },
+    { name: "Clutch Shoe 2 Stroke", image: "images/tu26/part_5.jpg" },
+    { name: "Clutch Drum 2 Stroke", image: "images/tu26/part_6.jpg" },
+    { name: "Muffler for TU-26", image: "images/tu26/part_7.jpg" },
+    { name: "Piston Ring for TU-26", image: "images/tu26/part_8.jpg" },
+    { name: "Crank Shaft TU-26", image: "images/tu26/part_9.jpg" },
+    { name: "Fly Wheel TU-26", image: "images/tu26/part_10.jpg" },
+    { name: "Normal Delivery Pipe", image: "images/tu26/part_11.jpg" },
+    { name: "Delivery Pipe with Twister", image: "images/tu26/part_12.jpg" },
+    { name: "60cm Band Brass Gun", image: "images/tu26/part_13.jpg" },
+    { name: "60cm Brass Gun", image: "images/tu26/part_14.jpg" },
+    { name: "90cm Brass Gun", image: "images/tu26/part_15.jpg" },
+    { name: "90cm Band Brass Gun", image: "images/tu26/part_16.jpg" },
+    { name: "TU-26 Starter Normal", image: "images/tu26/part_17.jpg" },
+    { name: "TU-26 Starter 6/4", image: "images/tu26/part_18.jpg" },
+    { name: "TU-26 Starter 4/4", image: "images/tu26/part_19.jpg" },
+    { name: "Throttle Lever Assy. New Type 767", image: "images/tu26/part_20.jpg" },
+    { name: "Throttle Lever Assy. 767", image: "images/tu26/part_21.jpg" },
+    { name: "8.5mm Roll (100m Roll)", image: "images/tu26/part_22.jpg" },
+    { name: "Gun Cap Set", image: "images/tu26/part_23.jpg" },
+    { name: "Stop Switch", image: "images/tu26/part_24.jpg" },
+    { name: "Earth Wire Red", image: "images/tu26/part_25.jpg" },
+    { name: "Petrol Filter", image: "images/tu26/part_26.jpg" },
+    { name: "Acc. Wire TU-26", image: "images/tu26/part_27.jpg" },
+    { name: "Left and Right Drainage Chamber", image: "images/tu26/part_28.jpg" },
+    { name: "Left and Right Cylinder Block", image: "images/tu26/part_29.jpg" },
+    { name: "Left and Right Inlet Chamber", image: "images/tu26/part_30.jpg" },
+    { name: "Acc. Lever Yellow & Black", image: "images/tu26/part_31.jpg" },
+    { name: "Red Handle", image: "images/tu26/part_32.jpg" },
+    { name: "Gland", image: "images/tu26/part_33.jpg" },
+    { name: "Grease Ring for Power Sprayer", image: "images/tu26/part_34.jpg" },
+    { name: "Insulator 2 Stroke", image: "images/tu26/part_35.jpg" },
+    { name: "Petrol Pipe Yellow", image: "images/tu26/part_36.jpg" },
+    { name: "Petrol Pipe Black", image: "images/tu26/part_37.jpg" },
+    { name: "Rubber for Petrol Pipe", image: "images/tu26/part_38.jpg" },
+    { name: "Petrol Pipe Assy.", image: "images/tu26/part_39.jpg" },
+    { name: "Italian Gun", image: "images/tu26/part_40.jpg" },
+    { name: "S Band Small", image: "images/tu26/part_41.jpg" },
+    { name: "S Band Big", image: "images/tu26/part_42.jpg" },
+    { name: "Big Cap Set for Lance", image: "images/tu26/part_43.jpg" },
+    { name: "Small Cap Set for Lance", image: "images/tu26/part_44.jpg" },
+    { name: "Pulley Starter Small", image: "images/tu26/part_45.jpg" },
+    { name: "Pulley Starter Big", image: "images/tu26/part_46.jpg" },
+    { name: "Plug Cap with Spring", image: "images/tu26/part_47.jpg" },
+    { name: "Punching Machine", image: "images/tu26/part_48.jpg" },
+    { name: "Valve Assy. 16,18mm", image: "images/tu26/part_49.jpg" },
+    { name: "Valve Assy 16,18mm", image: "images/tu26/part_50.jpg" },
+    { name: "Pressure Control Assy.", image: "images/tu26/part_51.jpg" },
+    { name: "Red Nozzle Small (Brass)", image: "images/tu26/part_52.jpg" },
+    { name: "Red Nozzle Big (Brass)", image: "images/tu26/part_53.jpg" },
+    { name: "Oil Seal for Crank 12*30*7", image: "images/tu26/part_54.jpg" },
+    { name: "Oil Seal for Plunger 18*28*8", image: "images/tu26/part_55.jpg" },
+    { name: "Oil Seal for Crank 12*22*7", image: "images/tu26/part_56.jpg" },
+    { name: "Oil Bubble", image: "images/tu26/part_57.jpg" },
+    { name: "White Ring", image: "images/tu26/part_58.jpg" },
+    { name: "V Packing 18,16mm Red/Black", image: "images/tu26/part_59.jpg" },
+    { name: "Valve Packing Red 16,18mm", image: "images/tu26/part_60.jpg" },
+    { name: "Valve Packing Black 16,18mm", image: "images/tu26/part_61.jpg" },
+    { name: "Gland Hard Ring", image: "images/tu26/part_62.jpg" },
+    { name: "3 Way Lance", image: "images/tu26/part_63.jpg" },
+    { name: "60cm Extension", image: "images/tu26/part_64.jpg" },
+    { name: "Plug Spanner", image: "images/tu26/part_65.jpg" },
+    { name: "Oil Glass", image: "images/tu26/part_66.jpg" },
+    { name: "Butter Cup", image: "images/tu26/part_67.jpg" },
+    { name: "SS Connector", image: "images/tu26/part_68.jpg" },
+    { name: "Elbow Big Brass", image: "images/tu26/part_69.jpg" },
+    { name: "Elbow Small Brass", image: "images/tu26/part_70.jpg" },
+    { name: "Long Absorber", image: "images/tu26/part_71.jpg" },
+    { name: "Short Absorber", image: "images/tu26/part_72.jpg" },
+    { name: "W4 Washer", image: "images/tu26/part_73.jpg" },
+    { name: "W6 Washer", image: "images/tu26/part_74.jpg" },
+    { name: "Wave Washer SS for Plunger", image: "images/tu26/part_75.jpg" },
+    { name: "Wave Washer 4 Stroke Clutch Bolt", image: "images/tu26/part_76.jpg" },
+    { name: "Wave Washer 2 Stroke Clutch Bolt", image: "images/tu26/part_77.jpg" },
+    { name: "Clutch Shoe Bolt with Washer", image: "images/tu26/part_78.jpg" },
+    { name: "Hose Clip 12mm", image: "images/tu26/part_79.jpg" },
+    { name: "Hose Clip 18mm", image: "images/tu26/part_80.jpg" },
+    { name: "Lock Nut", image: "images/tu26/part_81.jpg" },
+    { name: "Bolt Assy. for Belt", image: "images/tu26/part_82.jpg" },
+    { name: "Back Seat Screw", image: "images/tu26/part_83.jpg" },
+    { name: "Recoil Screw for TU-26", image: "images/tu26/part_84.jpg" },
+    { name: "Recoil Screw for Easy", image: "images/tu26/part_85.jpg" },
+    { name: "Gear Box Screw", image: "images/tu26/part_86.jpg" },
+    { name: "Cylinder Bolt", image: "images/tu26/part_87.jpg" },
+    { name: "Coil Screw", image: "images/tu26/part_88.jpg" },
+    { name: "Cylinder Bolt Short", image: "images/tu26/part_89.jpg" },
+    { name: "Cylinder Bolt Long", image: "images/tu26/part_90.jpg" },
+    { name: "Carb. Bolt", image: "images/tu26/part_91.jpg" },
+    { name: "Pump Bolt Long", image: "images/tu26/part_92.jpg" },
+    { name: "Bush SS and Non-SS", image: "images/tu26/part_93.jpg" }
+];
+
+const f139Parts = [
+    { name: "Carburetor 139-F", image: "images/139f/part_1.jpg" },
+    { name: "Crankcase 139-F", image: "images/139f/part_2.jpg" },
+    { name: "Crankshaft 139-F", image: "images/139f/part_3.jpg" },
+    { name: "Piston Assy. 139-F", image: "images/139f/part_4.jpg" },
+    { name: "Ignition Coil 139-F", image: "images/139f/part_5.jpg" },
+    { name: "Clutch Drum 4 Stroke", image: "images/139f/part_6.jpg" },
+    { name: "Clutch Shoe 4 Stroke", image: "images/139f/part_7.jpg" },
+    { name: "Tappet Valve 139-F", image: "images/139f/part_8.jpg" },
+    { name: "Muffler 139-F", image: "images/139f/part_9.jpg" },
+    { name: "Fly Wheel 139-F", image: "images/139f/part_10.jpg" },
+    { name: "Grey Starter 139-F", image: "images/139f/part_11.jpg" },
+    { name: "139-H Starter", image: "images/139f/part_12.jpg" },
+    { name: "Black Starter 139-F", image: "images/139f/part_13.jpg" },
+    { name: "Cam Wheel 139-F", image: "images/139f/part_14.jpg" },
+    { name: "Insulator 139-F", image: "images/139f/part_15.jpg" },
+    { name: "Acc Wire 139-F", image: "images/139f/part_16.jpg" },
+    { name: "Long Absorber", image: "images/139f/part_17.jpg" },
+    { name: "Short Absorber", image: "images/139f/part_18.jpg" },
+    { name: "Long Flat Absorber", image: "images/139f/part_19.jpg" },
+    { name: "Plug Cap with Spring", image: "images/139f/part_20.jpg" }
+];
+
+const gx35Parts = [
+    { name: "GX-35 Starter Normal", image: "images/gx35/part_1.jpg" },
+    { name: "GX-35 Easy Starter", image: "images/gx35/part_2.jpg" },
+    { name: "GX-35 Air Filter", image: "images/gx35/part_3.jpg" },
+    { name: "GX-35 Tappet Valve", image: "images/gx35/part_4.jpg" },
+    { name: "GX-35 Piston Assy.", image: "images/gx35/part_5.jpg" },
+    { name: "GX-35 Ignition Coil", image: "images/gx35/part_6.jpg" },
+    { name: "GX-35 Cam Wheel", image: "images/gx35/part_7.jpg" },
+    { name: "GX-35 Honda Filter Small", image: "images/gx35/part_8.jpg" },
+    { name: "GX-35 Honda Filter Big", image: "images/gx35/part_9.jpg" },
+    { name: "GX-35 Acc Wire", image: "images/gx35/part_10.jpg" },
+    { name: "Black Pipe Small", image: "images/gx35/part_11.jpg" },
+    { name: "Black Pipe Big", image: "images/gx35/part_12.jpg" },
+    { name: "GX-35 Oil Seal", image: "images/gx35/part_13.jpg" }
+];
+
+// Default fallback image if an image file is missing
+const defaultPartImage = "images/placeholder.jpg";
+
+// Helper function to render cards using object item properties
+function createCardsHTML(items) {
+    return items.map((item, idx) => `
+        <div class="part-card">
+            <div class="part-img-wrapper">
+                <img src="${item.image}" alt="${item.name}" loading="lazy" onerror="this.onerror=null;this.src='${defaultPartImage}';">
+            </div>
+            <div class="part-info">
+                <div class="item-num">ITEM #${idx + 1}</div>
+                <h4>${item.name}</h4>
+            </div>
+            <div class="card-actions">
+                <a href="tel:+919266769669" class="btn-sm-yellow">📞 CALL</a>
+                <a href="https://wa.me/919266769669?text=Hi,%20I%20am%20interested%20in%20buying:%20${encodeURIComponent(item.name)}" class="btn-sm-green" target="_blank">💬 WHATSAPP</a>
+            </div>
+        </div>
+    `).join('');
+}
+
+// Switch Active Pages
+function showPage(pageId) {
+    const pages = document.querySelectorAll('.page-content');
+    pages.forEach(page => page.classList.remove('active'));
+
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => link.classList.remove('active'));
+
+    const targetPage = document.getElementById('page-' + pageId);
+    if (targetPage) {
+        targetPage.classList.add('active');
+    }
+
+    const activeNav = document.getElementById('nav-' + pageId);
+    if (activeNav) {
+        activeNav.classList.add('active');
+    }
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    setTimeout(initScrollReveal, 50);
+}
+
+// Search Filter Functionality
+function filterParts(pageType) {
+    let inputId = '';
+    let parentSelector = '';
+
+    if (pageType === 'power-sprayer') {
+        inputId = 'search-power';
+        parentSelector = '#page-power-sprayer .part-card';
+    } else if (pageType === 'htp-pump') {
+        inputId = 'search-htp';
+        parentSelector = '#page-htp-pump .part-card';
+    } else if (pageType === 'battery-pump') {
+        inputId = 'search-battery';
+        parentSelector = '#page-battery-pump .part-card';
+    }
+
+    const filter = document.getElementById(inputId).value.toLowerCase();
+    const cards = document.querySelectorAll(parentSelector);
+
+    cards.forEach(card => {
+        const text = card.textContent || card.innerText;
+        card.style.display = text.toLowerCase().includes(filter) ? "" : "none";
+    });
+}
+
+// Scroll Reveal Observer
+function initScrollReveal() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    }, { threshold: 0.1 });
+
+    const revealElements = document.querySelectorAll('.feature-card, .category-card, .banner-cta, .c-card');
+    revealElements.forEach(el => {
+        el.classList.add('reveal-on-scroll');
+        observer.observe(el);
+    });
+}
+
+// Populate Grids & Init Animations on DOM Load
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("grid-battery").innerHTML = createCardsHTML(batteryParts);
+    document.getElementById("grid-htp").innerHTML = createCardsHTML(htpParts);
+    document.getElementById("grid-tu26").innerHTML = createCardsHTML(tu26Parts);
+    document.getElementById("grid-139f").innerHTML = createCardsHTML(f139Parts);
+    document.getElementById("grid-gx35").innerHTML = createCardsHTML(gx35Parts);
+
+    initScrollReveal();
+});
